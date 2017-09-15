89258e31e9e2a4b58b1aba2880707862222ebeec
glc12125 <glc12125@gmail.com>
2017-09-15 17:06:00 +0100

    add cloud viewer from binary files directly

---
+++ pcl/bin_read.cpp
@@ -0,0 +1,116 @@
+#include <boost/program_options.hpp>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/common/point_operators.h>
+#include <pcl/common/io.h>
+#include <pcl/search/organized.h>
+#include <pcl/search/octree.h>
+#include <pcl/search/kdtree.h>
+#include <pcl/features/normal_3d_omp.h>
+#include <pcl/filters/conditional_removal.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/segmentation/extract_clusters.h>
+#include <pcl/surface/gp3.h>
+#include <pcl/io/vtk_io.h>
+#include <pcl/filters/voxel_grid.h>
+
+#include <iostream>
+#include <fstream>
+
+using namespace pcl;
+using namespace std;
+
+namespace po = boost::program_options;
+
+int user_data;
+
+void
+viewerOneOff (pcl::visualization::PCLVisualizer& viewer)
+{
+    viewer.setBackgroundColor (1.0, 0.5, 1.0);
+    pcl::PointXYZ o;
+    o.x = 1.0;
+    o.y = 0;
+    o.z = 0;
+    viewer.addSphere (o, 0.25, "sphere", 0);
+    std::cout << "i only run once" << std::endl;
+
+}
+
+void
+viewerPsycho (pcl::visualization::PCLVisualizer& viewer)
+{
+    static unsigned count = 0;
+    std::stringstream ss;
+    ss << "Once per viewer loop: " << count++;
+    viewer.removeShape ("text", 0);
+    viewer.addText (ss.str(), 200, 300, "text", 0);
+
+    //FIXME: possible race condition here:
+    user_data++;
+}
+
+int main(int argc, char **argv){
+	///The file to read from.
+	string infile;
+
+	// Declare the supported options.
+	po::options_description desc("Program options");
+	desc.add_options()
+		//Options
+		("infile", po::value<string>(&infile)->required(), "the file to read a point cloud from")
+		;
+	// Parse the command line
+	po::variables_map vm;
+	po::store(po::parse_command_line(argc, argv, desc), vm);
+
+	// Print help
+	if (vm.count("help"))
+	{
+		cout << desc << "\n";
+		return false;
+	}
+
+	// Process options.
+	po::notify(vm);
+
+	// load point cloud
+	fstream input(infile.c_str(), ios::in | ios::binary);
+	if(!input.good()){
+		cerr << "Could not read file: " << infile << endl;
+		exit(EXIT_FAILURE);
+	}
+	input.seekg(0, ios::beg);
+
+	pcl::PointCloud<PointXYZI>::Ptr points (new pcl::PointCloud<PointXYZI>);
+
+	int i;
+	for (i=0; input.good() && !input.eof(); i++) {
+		PointXYZI point;
+		input.read((char *) &point.x, 3*sizeof(float));
+		input.read((char *) &point.intensity, sizeof(float));
+		points->push_back(point);
+	}
+	input.close();
+
+	cout << "Read KTTI point cloud with " << i << " points" << endl;
+    pcl::visualization::CloudViewer viewer("Cloud Viewer");
+
+    //blocks until the cloud is actually rendered
+    viewer.showCloud(points);
+
+//This will only get called once
+    viewer.runOnVisualizationThreadOnce (viewerOneOff);
+
+    //This will get called once per visualization iteration
+    viewer.runOnVisualizationThread (viewerPsycho);
+    while (!viewer.wasStopped ())
+    {
+    //you can also do cool processing here
+    //FIXME: Note that this is running in a separate thread from viewerPsycho
+    //and you should guard against race conditions yourself...
+    user_data++;
+    }
+    return 0;
+}
